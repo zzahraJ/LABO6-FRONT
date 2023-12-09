76858b38e76b8d321712cfa46d0b35dee4320a48
zzahraJ <r0883814@student.thomasmore.be>
2023-12-09 13:29:39 +0100

    feat: add the right link

--- src/components/UpdateScore.vue
+++ src/components/UpdateScore.vue
@@ -13,7 +13,7 @@ const teams = ref([
 ]);
 
 onMounted(() => {
-    socket = new WebSocket('https://labo6-back-a3tg.onrender.com/primus');
+    socket = new WebSocket('wss://labo6-back-a3tg.onrender.com/primus');
 });
 
 const updateScore = (team, score) => {
