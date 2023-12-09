9133afcc68f374f525257c2503abf39c2028d18f
zzahraJ <r0883814@student.thomasmore.be>
2023-12-09 13:19:52 +0100

    feat: import the components

--- src/main.js
+++ src/main.js
@@ -2,4 +2,24 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//import components
+import UpdateScore from './components/UpdateScore.vue'
+import Scoreboard from './components/Scoreboard.vue'
+
+//import vue router
+import * as VueRouter from 'vue-router'
+
+const routes = [
+    { path: '/', component: Scoreboard },
+    { path: '/updatescore', component: UpdateScore },
+]
+
+const router = VueRouter.createRouter({
+history: VueRouter.createWebHashHistory(),
+routes, 
+})
+
+const app = createApp(App) 
+app.use(router)
+
+app.mount('#app')
