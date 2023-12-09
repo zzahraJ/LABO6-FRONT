2a0c24bbcd0d3bee6c9975e38a22de3ba5bec834
zzahraJ <r0883814@student.thomasmore.be>
2023-12-09 13:19:32 +0100

    feat: add the update score file

---
+++ src/components/UpdateScore.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+
+let socket = null;
+
+// Initial teams with scores
+
+const teams = ref([
+  {team: 'De gele eendjes', score: 0},
+  {team: 'De blauwe eendjes', score: 0},
+  {team: 'De rode eendjes', score: 0},
+  {team: 'De groene eendjes', score: 0},
+]);
+
+onMounted(() => {
+    socket = new WebSocket('https://labo6-back-a3tg.onrender.com/primus');
+});
+
+const updateScore = (team, score) => {
+    socket.send(JSON.stringify({
+        action: 'updateScore', 
+        team: team, 
+        score: score
+    }));
+}
+
+</script>
+
+<template>
+    <div>
+    <h1>Update Score</h1>
+    <label for="teamSelector">Select your team</label>
+    <select v-model="team" id="teamSelector">
+        <option v-for="t in teamScores">
+          {{ t }}
+        </option>
+      </select>
+    <input type="number" v-model="score">
+    <button type="submit" @click="updateScore">Update Score</button>
+  </div>
+</template>
+
+<style scoped>
+
+
+</style>
